cf27cacdd2211867a2d83e85287fd96ec49d2acd
s <sharon.woo@outlook.com>
2023-04-21 13:42:35 +0800

    try out writing

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ from google.cloud import bigquery
 # Construct a BigQuery client object.
 client = bigquery.Client()
 
+table_id = "foodpanda-de-test-sharon.test_public_dataset.usa_people"
+
+job_config = bigquery.QueryJobConfig(destination=table_id, write_disposition="WRITE_TRUNCATE")
+
 query = """
     SELECT name, SUM(number) as total_people
     FROM `bigquery-public-data.usa_names.usa_1910_2013`
@@ -11,7 +15,7 @@ query = """
     ORDER BY total_people DESC
     LIMIT 20
 """
-query_job = client.query(query)  # Make an API request.
+query_job = client.query(query, job_config=job_config)  # Make an API request.
 query_job.result()  # Wait for the job to complete.
 
 print("The query data:")
